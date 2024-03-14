148af9473c34982c9fb0205ca615df9808f01360
zhaksylyk1 <zhk27aa@gmail.com>
2024-03-15 03:14:29 +0500

    upload+watch

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_video, name='upload_video'),  # For uploading videos
     path('', views.video_list, name='video_list'),  # For listing videos
     path('delete/<int:video_id>/', views.delete_video, name='delete_video'),  # URL pattern for the delete view
+    # path('upload/', views.upload_video, name='upload_video'),  # For uploading videos
+   
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import VideoForm
 from .models import Video
 
-def upload_video(request):
+# def upload_video(request):
+#     if request.method == 'POST':
+#         form = VideoForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('video_list')
+#     else:
+#         form = VideoForm()
+#     return render(request, 'myapp/upload_video.html', {'form': form})
+
+def video_list(request):
     if request.method == 'POST':
         form = VideoForm(request.POST, request.FILES)
         if form.is_valid():
@@ -10,17 +20,15 @@ def upload_video(request):
             return redirect('video_list')
     else:
         form = VideoForm()
-    return render(request, 'myapp/upload_video.html', {'form': form})
-
-def video_list(request):
     videos = Video.objects.all()  # Fetch all video objects from the database
-    time_points = [('Intro', 10), ('Middle', 30), ('End', 40)]
-    return render(request, 'myapp/video_list.html', {'videos': videos, 'time_points': time_points})
+    time_points = [('Intro', 10, 30), ('Middle', 30, 45), ('End', 45, 47)]
+    return render(request, 'myapp/video_list.html', {'videos': videos, 'time_points': time_points, 'form': form})
 
 def delete_video(request, video_id):
     if request.method == 'POST':  # Ensure the request method is POST
         video = Video.objects.get(id=video_id)  # Get the video to delete
         video.delete()  # Delete the video
         return redirect('video_list')  # Redirect to the video list page
+    
 
 
